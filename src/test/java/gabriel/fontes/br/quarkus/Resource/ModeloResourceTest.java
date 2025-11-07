c1bdb9f38ee4d8f79fa0ced2e6b6338bb680b222
BielGG9 <babielgg9@gmail.com>
2025-11-07 09:59:22 -0300

    COMEÇANDO E-COMMERCE

--- src/test/java/gabriel/fontes/br/quarkus/Resource/ModeloResourceTest.java
+++ src/test/java/gabriel/fontes/br/quarkus/Resource/ModeloResourceTest.java
@@ -9,17 +9,14 @@ import gabriel.fontes.br.quarkus.Dto.ModeloRequest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.equalToIgnoringCase; // Import para o ignoreCase (embora não vá resolver aqui)
 
 @QuarkusTest
 public class ModeloResourceTest {
 
-    // IDs baseados no import.sql. ID 1 = Corsair, ID 2 = Seasonic
     private static final Long EXISTING_MODELO_ID = 1L;
     private static final Long NON_EXISTING_MODELO_ID = 999L;
     
-    // --- CORREÇÃO AQUI ---
-    // Mudar de 1L para 2L, para usar "Seasonic" que não é modificada por outros testes.
+
     private static final Long MARCA_ID_PARA_TESTE = 2L; 
     private static final String NOME_MARCA_PARA_TESTE = "Seasonic";
 
@@ -54,7 +51,6 @@ public class ModeloResourceTest {
 
     @Test
     public void testCreateEndpoint() {
-        // Usa a Marca ID 2 ("Seasonic")
         ModeloRequest requestDto = new ModeloRequest(850, MARCA_ID_PARA_TESTE);
 
         given()
@@ -64,24 +60,23 @@ public class ModeloResourceTest {
                 .then()
                 .statusCode(201)
                 .body("numeracao", is(850))
-                .body("marca", is(NOME_MARCA_PARA_TESTE)); // Verifica por "Seasonic"
+                .body("marca", is(NOME_MARCA_PARA_TESTE)); 
     }
 
     @Test
     public void testUpdateEndpoint() {
-        // Usa a Marca ID 2 ("Seasonic")
         ModeloRequest requestDto = new ModeloRequest(900, MARCA_ID_PARA_TESTE);
 
         given()
                 .contentType(ContentType.JSON)
                 .body(requestDto)
-                .pathParam("id", EXISTING_MODELO_ID) // Atualiza o Modelo 1 (RM750)
+                .pathParam("id", EXISTING_MODELO_ID)
                 .when().put("/modelos/{id}")
                 .then()
                 .statusCode(200)
                 .body("id", is(EXISTING_MODELO_ID.intValue()))
                 .body("numeracao", is(900))
-                .body("marca", is(NOME_MARCA_PARA_TESTE)); // Verifica se a marca foi atualizada para "Seasonic"
+                .body("marca", is(NOME_MARCA_PARA_TESTE)); 
     }
 
     @Test
@@ -98,7 +93,7 @@ public class ModeloResourceTest {
 
     @Test
     public void testDeleteEndpoint() {
-        // Cria um modelo usando a Marca ID 2 ("Seasonic")
+
          ModeloRequest createDto = new ModeloRequest(1200, MARCA_ID_PARA_TESTE);
          var response = given()
                             .contentType(ContentType.JSON)
